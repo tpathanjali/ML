a87f37721b6ab05dd56ff25f786ad3e202f16794
tpathanjali <raja.pathanjali@gmail.com>
2019-03-28 01:02:52 +0530

    Update flask_in_progress.py
    added flask code at the end. it should be made as a separate file

--- flask_in_progress.py
+++ flask_in_progress.py
@@ -146,3 +146,24 @@ grid.fit(X_train, y_train)
 
 k=preprocessing()
 k.transform(X_train)
+
+from flask import Flask, jsonify
+app = Flask(__name__)
+@app.route('/predict', methods=['POST']) # Your API endpoint URL would consist /predict
+def predict():
+    if lr:
+        try:
+            json_ = request.json
+            query = pd.get_dummies(pd.DataFrame(json_))
+            query = query.reindex(columns=model_columns, fill_value=0)
+
+            prediction = list(lr.predict(query))
+
+            return jsonify({'prediction': prediction})
+
+        except:
+
+            return jsonify({'trace': traceback.format_exc()})
+    else:
+        print ('Train the model first')
+        return ('No model here to use')
